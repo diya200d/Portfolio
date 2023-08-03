3c5580eb19b7d709036b7188ddf6e58c81e12e68
Dnyaneshwari Khatiwada <137375437+diya200d@users.noreply.github.com>
2023-08-03 23:36:44 +0530

    Add files via upload

---
+++ pizza.js
@@ -0,0 +1,47 @@
+
+
+function getvalue(){
+    let a=document.getElementById("Name").value;
+    // 
+//     let d=document.getElementById("email").value;
+//     let e=document.getElementById("veg").value;
+//     let f=document.getElementById("non-veg").value;
+//     let g=document.getElementById("quantity").value;
+//     let h=document.getElementById("small").value;
+//     let i=document.getElementById("mdeium").value;
+//     let j=document.getElementById("large").value;
+//    let k=document.getElementById("sausage,mushrooms,olives,tomatoes").value;
+//    let l=document.getElementById("extra-cheese").value;
+    
+
+  
+     let array=['1011','1022','1033'];
+   
+ let c= document.getElementById("coupons").value;
+for(let i=0;i<=array.length;i++){
+     if(c==array[i]){
+        
+// document.getElementsById("Name").style.color="blue";
+     window.alert("Your Code is Correct ")
+
+
+        document.write("Name is : - "+a+"<br>")
+        document.write("Your Total bill is =",+200)
+        // document.write(" Address is  : - "+b)
+
+        let b=document.getElementById("address").value;
+        document.write(" Address : - "+b)
+
+        let d=document.getElementById("email").value;
+        document.write(" Email id : - "+d)
+        
+
+
+               }
+               }
+          }
+      
+    
+
+
+
